031c95c591eb0e083eb3201a9f903fc3f5392d06
Hyeon Kim <hkim399@my.bcit.ca>
2024-01-06 00:23:11 -0800

    added nav features and some background colors

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,40 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+const navLinks = document.querySelectorAll('nav a');
+const underline = document.querySelector('.underline');
+
+navLinks.forEach(link => {
+link.addEventListener('mouseenter', function() {
+    const linkRect = this.getBoundingClientRect();
+    underline.style.width = `${linkRect.width}px`;
+    underline.style.transform = `translateX(${linkRect.left}px) scaleY(1)`;
+});
+
+link.addEventListener('mouseleave', function() {
+        underline.style.width = '0';
+        underline.style.transform = 'translateX(0) scaleY(0)';
+    });
+});
+
+window.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+
+    navLinks.forEach(link => {
+        const targetSection = document.querySelector(link.getAttribute('href'));
+        const sectionTop = targetSection.offsetTop;
+        const sectionHeight = targetSection.offsetHeight;
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+            const linkRect = link.getBoundingClientRect();
+            underline.style.width = `${linkRect.width}px`;
+            underline.style.transform = `translateX(${linkRect.left}px) scaleY(1)`;
+        }
+    });
+});
+
+
 /* <!-- ////////////////       Projects        //////////////// --> */
 var proj1 = document.querySelector('#proj1');
 var proj2 = document.querySelector('#proj2');
